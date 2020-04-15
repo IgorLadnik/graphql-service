8c975be21902f2f488382fe2536f0ec59f56e45e
ladni <ladnik@hotmail.com>
2020-04-15 07:20:20 +0300

    rename to type

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ import { Logger } from "./logger";
             resolveFunc: (data, args, fieldFullPath) => {
                 const selectedUser = users[args.id];
                 data.actualObj.push(selectedUser);
-                data.creatingObj.push({ name: selectedUser.name, id: selectedUser.id });
+                data.creatingObj.push({ type: selectedUser.type, id: selectedUser.id });
             }
         },
         {
@@ -57,7 +57,7 @@ import { Logger } from "./logger";
             resolveFunc: (data, args, fieldFullPath) => {
                 for (let i = 0; i < 2; i++) {
                     data.actualObj.push(chats[i]);
-                    data.creatingObj.push({ name: chats[i].name, id: chats[i].id });
+                    data.creatingObj.push({ type: chats[i].type, id: chats[i].id });
                 }
             }
         },
@@ -71,24 +71,24 @@ export const Role = {
 
 // Test Data ------------------------------------------------------------------------------------
 export const users = [
-    { name: 'User', id: 0, username: 'Julius Verne', email: 'jv@MysteriousIsland.com', role: Role.Admin },
-    { name: 'User', id: 1, username: 'Cyrus Smith', email: 'cs@MysteriousIsland.com', role: Role.User },
-    { name: 'User', id: 2, username: 'Gedeon Spilett', email: 'gs@MysteriousIsland.com', role: Role.User },
+    { type: 'User', id: 0, name: 'Julius Verne', email: 'jv@MysteriousIsland.com', role: Role.Admin },
+    { type: 'User', id: 1, name: 'Cyrus Smith', email: 'cs@MysteriousIsland.com', role: Role.User },
+    { type: 'User', id: 2, name: 'Gedeon Spilett', email: 'gs@MysteriousIsland.com', role: Role.User },
 ];
 
 export const chatMessages = [
-    { name: 'ChatMessage', id: 0, content: 'aaaaaaa', time: Date.parse('2020-04-07'), author: users[0] },
-    { name: 'ChatMessage', id: 1, content: 'bbbbbbb', time: Date.parse('2020-04-07'), author: users[1] },
-    { name: 'ChatMessage', id: 2, content: 'ccccccc', time: Date.parse('2020-04-07'), author: users[0] },
-    { name: 'ChatMessage', id: 3, content: 'ddddddd', time: Date.parse('2020-04-07'), author: users[0] },
-    { name: 'ChatMessage', id: 4, content: 'eeeeeee', time: Date.parse('2020-04-07'), author: users[2] },
+    { type: 'ChatMessage', id: 0, content: 'aaaaaaa', time: Date.parse('2020-04-07'), author: users[0] },
+    { type: 'ChatMessage', id: 1, content: 'bbbbbbb', time: Date.parse('2020-04-07'), author: users[1] },
+    { type: 'ChatMessage', id: 2, content: 'ccccccc', time: Date.parse('2020-04-07'), author: users[0] },
+    { type: 'ChatMessage', id: 3, content: 'ddddddd', time: Date.parse('2020-04-07'), author: users[0] },
+    { type: 'ChatMessage', id: 4, content: 'eeeeeee', time: Date.parse('2020-04-07'), author: users[2] },
 ];
 
 export const chats = [
-    { name: 'Chat', id: 0, participants: [users[0], users[1], users[2]], messages: [chatMessages[0], chatMessages[1]] },
-    { name: 'Chat', id: 1, participants: [users[1], users[0]], messages: [chatMessages[1], chatMessages[2], chatMessages[3]] },
-    { name: 'Chat', id: 2, participants: [users[0], users[1]], messages: [chatMessages[4], chatMessages[1]] },
-    { name: 'Chat', id: 3, participants: [users[2], users[0]], messages: [chatMessages[4], chatMessages[1], chatMessages[2]] },
+    { type: 'Chat', id: 0, participants: [users[0], users[1], users[2]], messages: [chatMessages[0], chatMessages[1]] },
+    { type: 'Chat', id: 1, participants: [users[1], users[0]], messages: [chatMessages[1], chatMessages[2], chatMessages[3]] },
+    { type: 'Chat', id: 2, participants: [users[0], users[1]], messages: [chatMessages[4], chatMessages[1]] },
+    { type: 'Chat', id: 3, participants: [users[2], users[0]], messages: [chatMessages[4], chatMessages[1], chatMessages[2]] },
 ];
 // -----------------------------------------------------------------------------------------
 
@@ -103,7 +103,7 @@ query {
 
 query {
   user(id: 1) {
-	username
+	name
     id
   }
 }
@@ -111,7 +111,7 @@ query {
 query {
   allUsers {
 	role
-    username
+    name
     id
   }
 }
@@ -125,14 +125,14 @@ query {
 
 query {
   myChats {
-		id
+	id
     participants {
-      username
+      name
     }
     messages {
       author {
-      	username
-    	}
+      	name
+      }
       content
     }
   }
