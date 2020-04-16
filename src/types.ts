fc604eefb508211a4ed8842333d8c265cc5ec2bc
ladni <ladnik@hotmail.com>
2020-04-16 10:03:59 +0300

    extra comments removed

--- src/types.ts
+++ src/types.ts
@@ -7,26 +7,3 @@ export const Role = {
     User: 'USER',
     Admin: 'ADMIN'
 };
-
-// export type User = {
-//     type: 'User';
-//     id: number
-//     name: string;
-//     email: string;
-//     role: string;
-// }
-//
-// export type ChatMessage = {
-//     type: 'ChatMessage';
-//     id: number;
-//     content: string;
-//     time: number;
-//     author: User;
-// }
-//
-// export type Chat = {
-//     type: 'Chat';
-//     id: number;
-//     participants: Array<User>;
-//     messages: Array<ChatMessage>;
-// }
